4bd40da09cc28f730e5cb71596b6005ee537d940
Junseong Park <junseongpark.dev@gmail.com>
2023-04-10 14:32:09 +0900

    feat: add google analytics with partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify/functions";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), partytown()],
   output: "server",
   adapter: netlify()
 });
