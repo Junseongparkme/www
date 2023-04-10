185b83e0b0f9fdf3ecffb9264769354a373bcc8a
Junseong Park <junseongpark.dev@gmail.com>
2023-04-10 14:37:46 +0900

    fix: add missing setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify/functions";
@@ -7,7 +7,15 @@ import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), partytown()],
+  integrations: [
+    react(),
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
