f324af8ab13de58990e709ad55eced0da8b44455
Junseong Park <junseongpark.dev@gmail.com>
2023-04-25 14:15:35 +0900

    feat(config): add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
