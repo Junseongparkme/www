f90b405f5ce36b7831d35636ce5638b39700bcd8
Junseongparkme <junseongpark.dev@gmail.com>
2023-06-07 22:30:23 +0900

    feat(vercel): setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,15 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    tailwind(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
+  integrations: [react(), tailwind(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  })],
+  output: "server",
+  adapter: vercel()
 });
