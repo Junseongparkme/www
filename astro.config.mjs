07788468ae06e3de688c8c524b91947bea2fe3a6
Junseong Park <junseongpark.dev@gmail.com>
2023-06-27 16:49:58 +0900

    fix(analytics): remove

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,12 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import partytown from "@astrojs/partytown";
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    tailwind(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
+  integrations: [react(), tailwind()],
   output: "server",
   adapter: cloudflare(),
 });
